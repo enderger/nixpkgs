576a28104db33aa0060b33c3c019138764ba116a
Fabian Affolter <mail@fabian-affolter.ch>
2021-04-23 13:55:51 +0200

    python3Packages.nclib: init at 1.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4673,6 +4673,8 @@ in {
 
   ncclient = callPackage ../development/python-modules/ncclient { };
 
+  nclib = callPackage ../development/python-modules/nclib { };
+
   ndg-httpsclient = callPackage ../development/python-modules/ndg-httpsclient { };
 
   ndjson = callPackage ../development/python-modules/ndjson { };
