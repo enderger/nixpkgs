135efef80c6f57c449115acd21055ef82b277c25
Florian Franzen <Florian.Franzen@gmail.com>
2021-04-23 11:59:26 +0200

    sphinx: init from python3Packages.sphinx

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17517,6 +17517,8 @@ in
     fftw = fftwFloat;
   };
 
+  sphinx = with python3Packages; toPythonApplication sphinx;
+
   sphinx-serve = with python3Packages; toPythonApplication sphinx-serve;
 
   sphinxbase = callPackage ../development/libraries/sphinxbase { };
